db27a26430c756f030b1d4300c8b6271bf9b60f3
xxPicasso <ben@moregolf.co.nz>
2016-05-04 12:46:54 +1200

    woops forgot npm express local

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-import express from 'express'
-let app = express()
-let port = process.env.PORT || 8080
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 8080
 
 app.use(express.static(__dirname + '/public'))
+
+app.get('/', function (req, res) {
+	res.send("alligators crossing the road")
+})
 
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
